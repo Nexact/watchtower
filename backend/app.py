9ef92b3516b62ec5617ef36d5fe8e6dd2bbe1ab9
Nex <nex@sekodo.com>
2024-03-19 15:35:00 -0400

    adding bleepingcomputer

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from modules import lobsters
 from modules import lapresse
 from modules import radiocanada
 from modules import netsec
+from modules import bleepingcomputer
 
 import json
 
@@ -12,16 +13,18 @@ if __name__ == "__main__":
 
     hackernews_data = hackernews.get_hackernews()
     lobsters_data = lobsters.get_lobsters()
-    lapresse_data = lapresse.get_lapresse()
-    radiocanada_data = radiocanada.get_radiocanada()
+    #lapresse_data = lapresse.get_lapresse()
+    #radiocanada_data = radiocanada.get_radiocanada()
     netsec_data = netsec.get_netsec()
+    bleepingcomputer_data = bleepingcomputer.get_bleepingcomputer()
 
     # prepare json dump 
     data.append(hackernews_data)
     data.append(lobsters_data)
-    data.append(lapresse_data)
-    data.append(radiocanada_data)
+    #data.append(lapresse_data)
+    #data.append(radiocanada_data)
     data.append(netsec_data)
+    data.append(bleepingcomputer_data)
 
     print(json.dumps(data, indent=4))
 
